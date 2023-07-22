fb633624cd693653e5523cffd098c4ad22c3b727
Loic Mohin <lmohin@z2r1p5.42lyon.fr>
2023-07-22 03:45:50 +0200

    starting builtins

--- launching/launcher.c
+++ launching/launcher.c
@@ -6,7 +6,7 @@
 /*   By: TheTerror <jfaye@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/11 18:13:45 by TheTerror         #+#    #+#             */
-/*   Updated: 2023/07/11 18:22:16 by TheTerror        ###   ########lyon.fr   */
+/*   Updated: 2023/07/22 03:35:14 by lmohin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,11 @@
 
 t_bool	ft_launcher(t_vars *v)
 {
+	if (!strncmp("echo", v->argv[0], 5))
+	{
+		ft_echo(v, 0);
+		return (__TRUE);
+	}
 	ft_lnch_executable(v);
 	return (__TRUE);
 }
