af44aec69f6a07015e75c1502bf9d40173db23f2
Loic Mohin <lmohin@z1r1p2.42lyon.fr>
2023-08-17 04:48:14 +0200

    fix echo error

--- builtins/echo.c
+++ builtins/echo.c
@@ -6,7 +6,7 @@
 /*   By: lmohin <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/22 01:22:53 by lmohin            #+#    #+#             */
-/*   Updated: 2023/07/22 02:45:46 by lmohin           ###   ########.fr       */
+/*   Updated: 2023/08/17 00:03:54 by lmohin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,12 +19,12 @@ t_bool	ft_echo(t_vars *v, t_bool n_option)
 	i = 1 + n_option;
 	while (v->argv[i] != NULL)
 	{
-		printf("%s", v->argv[i]);
+		ft_putstr_fd(v->argv[i], 1);
 		i++;
 		if (v->argv[i] != NULL)
-			printf(" ");
+			ft_putchar_fd(' ', 1);
 	}
 	if (!n_option)
-		printf("\n");
+		ft_putchar_fd('\n', 1);
 	return (0);
 }
