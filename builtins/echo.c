30396223ccd3818a5721e6e82f347b3b8ad1350f
Loic Mohin <lmohin@z3r6p3.42lyon.fr>
2023-10-15 06:13:53 +0200

    fixing builtin echo

--- builtins/echo.c
+++ builtins/echo.c
@@ -6,7 +6,7 @@
 /*   By: lmohin <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/22 01:22:53 by lmohin            #+#    #+#             */
-/*   Updated: 2023/08/17 00:03:54 by lmohin           ###   ########.fr       */
+/*   Updated: 2023/10/15 06:07:38 by lmohin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,11 @@ t_bool	ft_echo(t_vars *v, t_bool n_option)
 	int	i;
 
 	i = 1 + n_option;
-	while (v->argv[i] != NULL)
+	while (v->commands->arguments[i] != NULL)
 	{
-		ft_putstr_fd(v->argv[i], 1);
+		ft_putstr_fd(v->commands->arguments[i], 1);
 		i++;
-		if (v->argv[i] != NULL)
+		if (v->commands->arguments[i] != NULL)
 			ft_putchar_fd(' ', 1);
 	}
 	if (!n_option)
