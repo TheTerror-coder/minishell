05e005fc4c307378d22ec927c44cd3b3ba5207b7
Loic Mohin <lmohin@z3r7p1.42lyon.fr>
2023-10-25 22:49:45 +0200

    update getpath

--- pathtools/getvalidpaths.c
+++ pathtools/getvalidpaths.c
@@ -6,7 +6,7 @@
 /*   By: TheTerror <jfaye@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/08 12:02:30 by TheTerror         #+#    #+#             */
-/*   Updated: 2023/10/22 17:07:18 by TheTerror        ###   ########lyon.fr   */
+/*   Updated: 2023/10/25 22:18:31 by lmohin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,52 +19,15 @@ t_bool	ft_add_slash(t_vars *v);
 t_bool	ft_set_path_variable(t_vars *v)
 {
 	char	*line;
-	line = ft_getlinepath(v);
+	line = check_env_var_set(v, "PATH");
 	if (!line)
 		return (ft_free2str(&v->paths), __TRUE);
 	v->paths = ft_split(line, ':');
 	if (!v->paths | !v->paths[0])
 		return (ft_leave(EXIT_FAILURE, \
 			"ft_set_path_variable() : ft_split() failed", __PRINT));
-	if (!ft_rm_path(v))
-		return (__FALSE);
 	if (!ft_add_slash(v))
 		return (__FALSE);
-	return (__TRUE);
-}
-
-char	*ft_getlinepath(t_vars *v)
-{
-	char	*line;
-	int		i;
-
-	i = 0;
-	line = NULL;
-	if (!v->envp || !v->envp[0])
-		return (ft_leave(EXIT_FAILURE, \
-		"minishell: environ fault: envp variable is not set", __PRINT), NULL);
-	while (v->envp[i])
-	{
-		line = ft_strnstr(v->envp[i], "PATH=", 7);
-		if (line)
-			return (v->envp[i]);
-		i++;
-	}
-	return (NULL);
-}
-
-t_bool	ft_rm_path(t_vars *v)
-{
-	char	**str;
-
-	str = ft_split(v->paths[0], '=');
-	if (!str)
-		return (ft_leave(EXIT_FAILURE, \
-			"ft_rm_path() : ft_split() failed", PRINT_ERROR));
-	ft_freestr(&v->paths[0]);
-	v->paths[0] = str[1];
-	str[1] = NULL;
-	ft_free2str(&str);
 	return (__TRUE);
 }
 
