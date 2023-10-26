eac589facf62c1a7e6eff1cd442b3060f97b9ec2
Loic Mohin <lmohin@z3r8p1.42lyon.fr>
2023-10-26 03:32:56 +0200

    simplify command_args

--- commandtools/command_args.c
+++ commandtools/command_args.c
@@ -6,12 +6,12 @@
 /*   By: lmohin <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/10 01:57:18 by lmohin            #+#    #+#             */
-/*   Updated: 2023/10/14 00:08:08 by lmohin           ###   ########.fr       */
+/*   Updated: 2023/10/26 03:31:14 by lmohin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
-
+/*
 void	add_arg(t_token **tokens, t_token *previous_token, t_commands *cmds)
 {
 	if (previous_token != NULL)
@@ -50,6 +50,53 @@ int	fill_command_arguments(t_commands *commands)
 	}
 	(commands->arguments)[arg_c] = NULL;
 	return (0);
+}*/
+
+void	fill_command_arguments(t_commands *commands)
+{
+	size_t	arg_c;
+	t_token	*tokens_cpy;
+	t_token	*tokens_redir;
+	t_bool	first_redir;
+	t_token	*start_tokens_redir;
+	t_token	*old_cpy;
+
+	arg_c = 0;
+	first_redir = __TRUE;
+	start_tokens_redir = NULL;
+	tokens_cpy = commands->tokens;
+	while (tokens_cpy != NULL)
+	{
+		if (tokens_cpy->type == 0)
+		{
+			(commands->arguments)[arg_c] = (tokens_cpy)->content;
+			arg_c++;
+			old_cpy = tokens_cpy;
+			tokens_cpy = tokens_cpy->next;
+			free(old_cpy);
+			old_cpy = NULL;
+		}
+		else
+		{
+			if (first_redir == __TRUE)
+			{
+				start_tokens_redir = tokens_cpy;
+				tokens_redir = tokens_cpy;
+				tokens_redir->next = tokens_cpy->next;
+				tokens_redir = tokens_redir->next;
+				first_redir = __FALSE;
+			}
+			else
+			{
+				tokens_redir->next = tokens_cpy;
+				tokens_redir->next->next = tokens_cpy->next;
+				tokens_redir = tokens_redir->next->next;
+			}
+			tokens_cpy = tokens_cpy->next->next;
+		}
+	}
+	commands->tokens = start_tokens_redir;
+	commands->arguments[arg_c] = NULL;
 }
 
 int	get_command_arguments(t_commands *commands)
@@ -69,6 +116,7 @@ int	get_command_arguments(t_commands *commands)
 		else
 			tokens_cpy = tokens_cpy->next->next;
 	}
+	commands->arguments = NULL;
 	commands->arguments = malloc(sizeof(char *) * (args_nbr + 1));
 	fill_command_arguments(commands);
 	return (0);
