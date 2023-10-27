864f3517c89112683205810d4578f9cb09f0eac3
Loic Mohin <lmohin@z1r10p1.42lyon.fr>
2023-10-27 02:26:32 +0200

    fix echo with multiple -nnnnn

--- builtins/echo.c
+++ builtins/echo.c
@@ -6,17 +6,39 @@
 /*   By: TheTerror <jfaye@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/22 01:22:53 by lmohin            #+#    #+#             */
-/*   Updated: 2023/10/20 00:42:04 by lmohin           ###   ########.fr       */
+/*   Updated: 2023/10/27 02:25:14 by lmohin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
-t_bool	ft_echo(t_commands *command, t_bool n_option)
+t_bool	is_n_flag(char	*s)
 {
-	int	i;
+	size_t	i;
 
-	i = 1 + n_option;
+	if (s[0] != '-')
+		return (__FALSE);
+	if (s[1] != 'n')
+		return (__FALSE);
+	i = 2;
+	while (s[i] != '\0')
+	{
+		if (s[i] != 'n')
+			return (__FALSE);
+		i++;
+	}
+	return (__TRUE);
+}
+
+t_bool	ft_echo(t_commands *command)
+{
+	t_bool	n_flag;
+	size_t	i;
+
+	n_flag = is_n_flag(command->arguments[1]);
+	i = 1;
+	while (is_n_flag(command->arguments[i]))
+		i++;
 	while (command->arguments[i] != NULL)
 	{
 		ft_putstr_fd(command->arguments[i], 1);
@@ -24,7 +46,7 @@ t_bool	ft_echo(t_commands *command, t_bool n_option)
 		if (command->arguments[i] != NULL)
 			ft_putchar_fd(' ', 1);
 	}
-	if (!n_option)
+	if (!n_flag)
 		ft_putchar_fd('\n', 1);
 	exitstatus = 0;
 	return (__TRUE);
