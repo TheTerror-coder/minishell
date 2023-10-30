fc548c89c26253c1e5e3ef079eba6385f28f1801
Loic Mohin <lmohin@z4r12p5.42lyon.fr>
2023-10-30 10:11:42 +0100

    clean setenv

--- envtools/setenv.c
+++ envtools/setenv.c
@@ -6,41 +6,23 @@
 /*   By: TheTerror <jfaye@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/26 04:26:44 by lmohin            #+#    #+#             */
-/*   Updated: 2023/10/30 09:59:24 by lmohin           ###   ########.fr       */
+/*   Updated: 2023/10/30 10:09:07 by lmohin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
-void	add_to_end_of_env(t_env *env, t_env *new)
-{
-	while (env->next != NULL)
-		env = env->next;
-	env->next = new;
-}
+t_bool	get_old_env(t_vars *v, char **envp);
+t_bool	set_pwd(t_vars *v);
+t_bool	add_env_var(t_vars *v, char *var);
+void	add_to_end_of_env(t_env *env, t_env *new);
 
-t_bool	add_env_var(t_vars *v, char *var)
+t_bool	ft_setenv(t_vars *v, char **envp)
 {
-	t_env	*new;
-
-	if (!v->my_env->var)
-	{
-		v->my_env->var = ft_strdup(var);
-		if (!(v->my_env->var))
-			return (ft_leave(v, EXIT_FAILURE, "ft_strdup", __PERROR), __FALSE);
-		return (__TRUE);
-	}
-	new = malloc(sizeof(t_env));
-	if (!new)
-		return (ft_leave(v, EXIT_FAILURE, "malloc", __PERROR), __FALSE);
-	new->next = NULL;
-	new->var = ft_strdup(var);
-	if (!(new->var))
-	{
-		free(new);
-		return (ft_leave(v, EXIT_FAILURE, "ft_strdup", __PERROR), __FALSE);
-	}
-	add_to_end_of_env(v->my_env, new);
+	if ((envp[0]) && !get_old_env(v, envp))
+		return (free_env(v), __FALSE);
+	if (!set_pwd(v))
+		return (free_env(v), __FALSE);
 	return (__TRUE);
 }
 
@@ -78,11 +60,34 @@ t_bool	set_pwd(t_vars *v)
 	return (free(env_pwd), __TRUE);
 }
 
-t_bool	ft_setenv(t_vars *v, char **envp)
+t_bool	add_env_var(t_vars *v, char *var)
 {
-	if ((envp[0]) && !get_old_env(v, envp))
-		return (free_env(v), __FALSE);
-	if (!set_pwd(v))
-		return (free_env(v), __FALSE);
+	t_env	*new;
+
+	if (!v->my_env->var)
+	{
+		v->my_env->var = ft_strdup(var);
+		if (!(v->my_env->var))
+			return (ft_leave(v, EXIT_FAILURE, "ft_strdup", __PERROR), __FALSE);
+		return (__TRUE);
+	}
+	new = malloc(sizeof(t_env));
+	if (!new)
+		return (ft_leave(v, EXIT_FAILURE, "malloc", __PERROR), __FALSE);
+	new->next = NULL;
+	new->var = ft_strdup(var);
+	if (!(new->var))
+	{
+		free(new);
+		return (ft_leave(v, EXIT_FAILURE, "ft_strdup", __PERROR), __FALSE);
+	}
+	add_to_end_of_env(v->my_env, new);
 	return (__TRUE);
+}
+
+void	add_to_end_of_env(t_env *env, t_env *new)
+{
+	while (env->next != NULL)
+		env = env->next;
+	env->next = new;
 }
