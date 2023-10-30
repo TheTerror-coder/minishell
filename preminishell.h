2fe5583cb0246b5714ce3320ee4f938a0996255c
Loic Mohin <lmohin@z1r2p2.42lyon.fr>
2023-10-30 08:20:49 +0100

    norming headers

--- preminishell.h
+++ preminishell.h
@@ -6,22 +6,26 @@
 /*   By: TheTerror <jfaye@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/08 16:04:51 by TheTerror         #+#    #+#             */
-/*   Updated: 2023/10/29 19:58:02 by TheTerror        ###   ########lyon.fr   */
+/*   Updated: 2023/10/30 08:20:25 by lmohin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PREMINISHELL_H
 # define PREMINISHELL_H
 
-#include "libft/libft.h"
+# include "libft/libft.h"
 
 # define __SKIP 2
 # define __CONTINUE 5
 # define __WHANG 0
 # define __PRINT PRINT_ERROR
 # define __PERROR PUT_ERROR
-# define __CLOSED_FD -111 // arbitrary negative number to initialize a file descriptor variable
-# define __MAX_ATTEMPTS 9999999 // number of possible names of the temporary file
+
+// arbitrary negative number to initialize a file descriptor variable
+# define __CLOSED_FD -111
+
+// number of possible names of the temporary file
+# define __MAX_ATTEMPTS 9999999
 # define __EXIT_REACHED 7
 # define __CMD_NOT_FOUND 127
 # define __CMD_NOT_EXEC 126
@@ -37,19 +41,19 @@ typedef enum e_typ
 
 typedef struct s_env
 {
-	char		*var;
-	struct	s_env 	*next;
+	char			*var;
+	struct s_env	*next;
 }			t_env;
 
 typedef struct s_token
 {
-	char		*content;
-	int			type;
-	int		expand_in_hdoc;
-	struct s_token *next;
+	char			*content;
+	int				type;
+	int				expand_in_hdoc;
+	struct s_token	*next;
 }			t_token;
 
-typedef	struct s_commands
+typedef struct s_commands
 {
 	char				*main_command;
 	char				**arguments;
@@ -58,7 +62,7 @@ typedef	struct s_commands
 	struct s_commands	*next;
 }			t_commands;
 
-typedef struct	s_ppex
+typedef struct s_ppex
 {
 	t_commands	*commands;
 	t_commands	*iterator;
@@ -75,28 +79,28 @@ typedef struct	s_ppex
 
 typedef struct s_vars
 {
-	t_env	*my_env;
-	t_commands *commands;
-	char	*cmdpath;
-	char	*pwd;
-	char	*line;
-	char	**paths;
-	char	*str;
-	char	*ftemp1;
-	char	*limiter;
-	int		exitstatus;
-	int		code;
-	int		p1[2];
-	int		p2[2];
-	int		infd;
-	int		outfd;
-	int		stdin;
-	int		stdout;
-	int		hdoc_fd;
-	t_bool	flg_var_is_null;
-	t_bool	flg_parsing_is_ok;
-	t_bool	flg_expand_in_hdoc;
-	t_bool	flg_exit_main_procss;
-	t_ppex	*var;
+	t_env		*my_env;
+	t_commands	*commands;
+	char		*cmdpath;
+	char		*pwd;
+	char		*line;
+	char		**paths;
+	char		*str;
+	char		*ftemp1;
+	char		*limiter;
+	int			exitstatus;
+	int			code;
+	int			p1[2];
+	int			p2[2];
+	int			infd;
+	int			outfd;
+	int			stdin;
+	int			stdout;
+	int			hdoc_fd;
+	t_bool		flg_var_is_null;
+	t_bool		flg_parsing_is_ok;
+	t_bool		flg_expand_in_hdoc;
+	t_bool		flg_exit_main_procss;
+	t_ppex		*var;
 }				t_vars;
 #endif
